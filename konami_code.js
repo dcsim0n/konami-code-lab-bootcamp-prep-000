592c76c3b588f4115b0a987aa408dc5c08ed671a
dsanalog <dsanalog@gmail.com>
2019-01-05 22:33:57 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,23 @@ const codes = [
   "b",
   "a"
 ];
+var counter = 0;
 
+var checkit = function(){
+  const key = this.key;
+  //console.log(key);
+  if (key === codes[counter]){
+    counter = counter + 1;
+    if(counter === codes.length){
+      alert("Haduka!");
+      counter = 0;
+    }
+  } else {
+    counter = 0;
+  }
+}
 function init() {
-  // your code here
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', checkit);
+  
 }
